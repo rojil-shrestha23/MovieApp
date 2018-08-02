0fa1cc1388fd66efdf3b9db041b012e793ee8011
rojil.shrestha <rojil.shrestha@wlink.com.np>
2018-08-02 23:26:11 +0545

    initilazed recycler view and layout manager

--- app/src/main/java/np/com/shrestha/rojil/moviedemo/MainActivity.java
+++ app/src/main/java/np/com/shrestha/rojil/moviedemo/MainActivity.java
@@ -2,12 +2,18 @@ package np.com.shrestha.rojil.moviedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView rvMoviesLists;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvMoviesLists = findViewById(R.id.rv_example_view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
+        rvMoviesLists.setLayoutManager(linearLayoutManager);
+        rvMoviesLists.setAdapter(new MovieListingAdapter(getMovieList()));
     }
 }
